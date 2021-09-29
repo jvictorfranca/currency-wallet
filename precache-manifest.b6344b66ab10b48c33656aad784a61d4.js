b9c333898a433dc4e83b82c16c2068d99a3e45ca
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-09-29 18:22:08 -0300

    Updates

---
+++ precache-manifest.b6344b66ab10b48c33656aad784a61d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa4c71c5ea16dc66befaa14e9907048",
+    "url": "/currency-wallet/index.html"
+  },
+  {
+    "revision": "d280993101c09c6c7d21",
+    "url": "/currency-wallet/static/css/main.726f9e79.chunk.css"
+  },
+  {
+    "revision": "3fa9001fb8670f3bde7a",
+    "url": "/currency-wallet/static/js/2.f00acdc6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/currency-wallet/static/js/2.f00acdc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d280993101c09c6c7d21",
+    "url": "/currency-wallet/static/js/main.03f874c7.chunk.js"
+  },
+  {
+    "revision": "bb01a6d43ba49fe5ddd4",
+    "url": "/currency-wallet/static/js/runtime-main.8f090066.js"
+  },
+  {
+    "revision": "aca7ff172782a466ad2bf99f9135bf26",
+    "url": "/currency-wallet/static/media/dark-leather.aca7ff17.png"
+  },
+  {
+    "revision": "5ea122d652c064d2124699bf0fb0ee54",
+    "url": "/currency-wallet/static/media/wallet.5ea122d6.png"
+  }
+]);
